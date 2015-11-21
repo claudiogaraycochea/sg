0b4012d794eee6d75e544ea1d15d39f8ec6256d2
Claudio Garaycochea <computadoraweb@gmail.com>
2015-11-21 15:36:24 -0300

    Modal for system

--- SGAppWebMobileTest/assets/js/app.js
+++ SGAppWebMobileTest/assets/js/app.js
@@ -1,21 +1,19 @@
-//pathBase='http://localhost/superguest/project/SGAppWebMobileTest/';
-pathBase='http://192.168.1.36/superguest/project/SGAppWebMobileTest/';
+pathBase='http://192.168.1.35/superguest/project/SGAppWebMobileTest/';
+//pathBase='http://hiresto.com/sg/';
 
 function targetModule(){
   $(function(){
     var src=pathBase+'app/system/module/moduleView.html';
-    //$('<iframe id="targetURL" src="'+src+'" class="target-URL hidden"/>').appendTo('body');
     $('<iframe id="targetURL" src="'+src+'" class="target-URL hidden"/>').appendTo('body');
     $('.target-URL').animate({left: 1000}, 300, function() {});
   });
 }
 
 function openModule(link){
-  $('.wrapper-aside').animate({left: "-=80%"}, 300, function() {
+  $('#wrapper-aside').animate({left: "-=80%"}, 300, function() {
     $('#targetURL').removeClass("hidden");
     $('.target-URL').animate({left: 0}, 300, function() {});
   });
-
 }
 
 function closeModule(){
@@ -29,33 +27,15 @@ function redefineTarget(){
   
   $('a').on('click', function(event) {
     event.preventDefault();
-    /*
-      This function is used to open a link
-      
-      target:
-        - modal   // open a modal with link 
-            eg:
-             <a href="reserve" target="modal"> 
-
-        - module    // open a module
-        - url   // open a url external
-
-
-    */
     var link = $(this).attr('href'); 
     var target = $(this).attr('target');
 
     switch(target) {
       case 'modal':
-        alert('abre MODAL '+link);
+        openModal(link);
         break;
       case 'module':
         openModule(link);
-        break;
-      case 'url':
-        var link=pathBase+"/app/components/"+link
-        alert(link);
-        window.open(link);
         break;
       default:
         window.open(link);
@@ -64,15 +44,30 @@ function redefineTarget(){
   });
 }
 
+function openModal(link){
+  if($('#modal-content').load(link)){
+    $('#modal-wrapper').removeClass("hidden");
+  }       
+}
+
+function closeModal(){
+  $('#modal-content').html('');
+  $('#modal-wrapper').attr('style','display:none;');
+}
+
 $(document).ready(function(){
   redefineTarget();
 
   $('.btn-icon-menu').on('click', function () {
-    $('.wrapper-aside').animate({left: "+=80%"}, 300, function() {});
+    $('#wrapper-aside').animate({left: "+=80%"}, 300, function() {});
   });
   
   $('.wrapper-content').on('click', function () {
-    $('.wrapper-aside').animate({left: "-=80%"}, 300, function() {});
+    $('#wrapper-aside').animate({left: "-=80%"}, 300, function() {});
   });
 
+  $('#modal-header-close').click(function(){
+    $('#modal-wrapper').addClass("hidden");
+  });
+    
 });
